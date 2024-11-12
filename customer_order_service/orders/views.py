d80ac0a24cfaeaf9c88b8b5a2668a5b62d805c94
ArthurJuma98 <arthurjuma98@gmail.com>
2024-11-12 19:47:46 +0300

    API Views

--- customer_order_service/orders/views.py
+++ customer_order_service/orders/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Customer, Order
+from .serializers import CustomerSerializer, OrderSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+"""API views for handling CRUD operations on Customer and Order objects"""
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        order = serializer.save()
+        send_sms_alert(order.customer.name, order.item)
+
