ecf073e281e37eb9f572bf20763bb13ab61d3c38
ArthurJuma98 <arthurjuma98@gmail.com>
2024-11-13 11:49:06 +0300

    model tests integration

--- customer_order_service/orders/tests.py
+++ customer_order_service/orders/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Customer, Order
+from rest_framework.test import APIClient
 
-# Create your tests here.
+class CustomerOrderTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.customer = Customer.objects.create(name="Brain Opiyo", code="21872")
+        self.order = Order.objects.create(customer=self.customer, item="Laptop", amount=1200)
+
+    def test_create_customer(self):
+        response = self.client.post('/api/customers/', {'name': 'Lilian Akinyi', 'code': '67890'})
+        self.assertEqual(response.status_code, 201)
+
+    def test_create_order(self):
+        response = self.client.post('/api/orders/', {'customer': self.customer.id, 'item': 'Phone', 'amount': 500})
+        self.assertEqual(response.status_code, 201)
